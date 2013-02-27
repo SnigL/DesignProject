58bf8e090d035f057ee91b0b3ca55a79896dced5
Filip <filip.grojer@gmail.com>
2013-02-27 19:57:26 +0100

    New icons for launcher

--- gen/com/example/designproject/R.java
+++ gen/com/example/designproject/R.java
@@ -25,9 +25,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int launcher_add=0x7f020001;
+        public static final int launcher_settings=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int action_settings=0x7f090002;
+        public static final int clockButton=0x7f090001;
+        public static final int settingsButton=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
